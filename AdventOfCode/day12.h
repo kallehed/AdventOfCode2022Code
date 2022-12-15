5a96041f741b8a276fee626a8bb6c20782d7da86
kallehed <kallehed@gmail.com>
2022-12-15 08:28:12 +0100

    Tried more at day 12

--- AdventOfCode/day12.h
+++ AdventOfCode/day12.h
@@ -20,25 +20,51 @@ constexpr int8_t HEIGHT = 41;
 using Form = std::array<std::array<bool, WIDTH>, HEIGHT>;
 using mapForm = std::array<std::array<char, WIDTH>, HEIGHT>;
 
-int recur(Form& where_been, Pos wher, mapForm& map, int steps, Pos& end)
+void print_where_been(Form& where_been) {
+    for (int i = 0; i < HEIGHT; ++i) {
+        for (int j = 0; j < WIDTH; ++j) {
+            std::cout << where_been[i][j];
+        }
+        std::cout << '\n';
+    }
+}
+
+int recur(Form& where_been, Pos wher, mapForm& map, Pos& end)
 {
+    if (wher.x == end.x && wher.y == end.y) {
+        //if (steps < 100) std::cout << steps << '\n';
+        
+        //if (steps == 437) {
+            //print_where_been(where_been);
+        //}
+          
+        return 0;
+    }
+
     int least_steps = INT_MAX;
-    constexpr std::array<std::array<int8_t, 2>, 4> ways = { {{1,0},{0,-1},{0,1},{-1,0}} };
-    if (wher.x == end.x && wher.y == end.y) { std::cout << steps << '\n'; return steps; }
+
+    constexpr std::array<Pos, 4> ways = { { {1,0},{0,1},{0,-1},{-1,0} } };
 
     for (auto& way : ways) {
-        Pos n_pos = { wher.x + way[0], wher.y + way[1] };
+        Pos n_pos = { wher.x + way.x, wher.y + way.y };
         if (n_pos.x >= 0 && n_pos.x < WIDTH && n_pos.y >= 0 && n_pos.y < HEIGHT) {
-            if (!where_been[n_pos.y][n_pos.x] && ((map[n_pos.y][n_pos.x] - 1) <= map[wher.y][wher.x]) && ((map[n_pos.y][n_pos.x] + 2) >= map[wher.y][wher.x])) {
+            if (!where_been[n_pos.y][n_pos.x] && (map[wher.y][wher.x] + 1 >= map[n_pos.y][n_pos.x])) {
                 where_been[n_pos.y][n_pos.x] = true;
-                int took = recur(where_been, n_pos, map, steps + 1, end);
-                if (took < least_steps) { least_steps = took; }
-                where_been[n_pos.y][n_pos.x] = false;
+                int took = recur(where_been, n_pos, map, end);
+                if (took == -1) {
+
+                }
+                else {
+                    if (took < least_steps) { least_steps = 1 + took; }
+                    where_been[n_pos.y][n_pos.x] = false;
+                }
             }
         }
     }
 
+    if (least_steps == INT_MAX) { return -1; }
     return least_steps;
+
 }
 
 void day12_1()
@@ -68,5 +94,5 @@ void day12_1()
 
     map[end.y][end.x] = 'z';
 
-    std::cout << recur(start_form, start, map, 0, end);
+    std::cout << recur(start_form, start, map, end);
 }
